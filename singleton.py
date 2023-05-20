08a26981daaffefbd41f585af52d9cbef96e40dd
sadpatheticboy <bal-tim@mail.ru>
2023-05-20 20:55:23 +0300

    Паттерн проектирования Singleton/Одиночка

---
+++ singleton.py
@@ -0,0 +1,12 @@
+class Singleton:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+
+a = Singleton()
+b = Singleton()
+print(a is b)

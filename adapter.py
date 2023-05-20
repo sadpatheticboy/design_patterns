a3a0d68c4e31fd7b3f6a0c957df4f0b9e69e95c4
sadpatheticboy <bal-tim@mail.ru>
2023-05-20 16:32:12 +0300

    Паттерн проектирования Адаптер

---
+++ adapter.py
@@ -0,0 +1,25 @@
+# JSON - str
+# XML - int
+
+class Old:
+    def get(self):
+        return '1234'
+
+
+class New:
+    def get_int(self):
+        return 456
+
+
+class Adapter(New):
+    def get(self):
+        return str(self.get_int())
+
+
+def main(obj):
+    print('Результат:' + obj.get())
+
+
+if __name__ == '__main__':
+    obj = Adapter()
+    main(obj)
